26ab52613af0f5fe1f9c36fe4696d55696d210b8
Лезовский Станислав <lezsv@reso.ru>
2024-03-26 09:34:22 +0300

    Notifications Permission

--- Sources/PermissionPackage/PermissionPackage.swift
+++ Sources/PermissionPackage/PermissionPackage.swift
@@ -115,6 +115,52 @@ public class FaceIDPermission: Permission {
     }
 }
 
+public extension Permission {
+    
+    static var notification: NotificationPermission {
+        return NotificationPermission()
+    }
+}
+
+public class NotificationPermission: Permission {
+    
+    open override var kind: Permission.Kind { .notification }
+    
+    public override var status: Permission.Status {
+        guard let authorizationStatus = fetchAuthorizationStatus() else { return .notDetermined }
+        switch authorizationStatus {
+        case .authorized: return .authorized
+        case .denied: return .denied
+        case .notDetermined: return .notDetermined
+        case .provisional: return .authorized
+        case .ephemeral: return .authorized
+        @unknown default: return .denied
+        }
+    }
+    
+    private func fetchAuthorizationStatus() -> UNAuthorizationStatus? {
+        var notificationSettings: UNNotificationSettings?
+        let semaphore = DispatchSemaphore(value: 0)
+        DispatchQueue.global().async {
+            UNUserNotificationCenter.current().getNotificationSettings { setttings in
+                notificationSettings = setttings
+                semaphore.signal()
+            }
+        }
+        semaphore.wait()
+        return notificationSettings?.authorizationStatus
+    }
+    
+    public override func request(completion: @escaping () -> Void) {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options:[.badge, .alert, .sound]) { (granted, error) in
+            DispatchQueue.main.async {
+                completion()
+            }
+        }
+    }
+}
+
 
 open class Permission {
     
